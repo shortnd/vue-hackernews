b68aab1abcd5dc48d077a714db115666df70428e
shortnd <ococncol@gmail.com>
2020-10-24 18:15:44 -0400

    adding e2e tests

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,17 +1,23 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import Item from '../Item.vue'
-import mergeWith from 'lodash.mergewith'
+// import mergeWith from 'lodash.mergewith'
+import { mergeWith } from 'lodash'
 
 describe('Item.vue', () => {
   function createWrapper (overrides) {
-    const defaultProps = { propsData: {} }
+    const defaultProps = {
+      propsData: {},
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    }
     return shallowMount(Item, mergeWith(defaultProps, overrides))
   }
   test('renders item.url', () => {
     const item = {
       url: 'https://some-url.com/with-paths'
     }
-    const wrapper = shallowMount(Item, {
+    const wrapper = createWrapper({
       propsData: { item }
     })
     expect(wrapper.text()).toContain('(some-url.com)')
@@ -51,12 +57,54 @@ describe('Item.vue', () => {
     const item = {
       time: (dateNowTime / 1000) - 600
     }
-    const wrapper = shallowMount(Item, {
+    const wrapper = createWrapper({
       propsData: {
         item
       }
     })
     dateNow.mockRestore()
     expect(wrapper.text()).toContain('10 minutes ago')
+  })
+
+  test('renders correctly', () => {
+    const dateNow = jest.spyOn(Date, 'now')
+    const dateNowTime = new Date('2018')
+
+    dateNow.mockImplementation(() => dateNowTime)
+
+    const item = {
+      by: 'eddyerburgh',
+      id: 11122233,
+      score: 10,
+      time: (dateNowTime / 1000) - 600,
+      title: 'vue-test-utils is released',
+      type: 'story',
+      url: 'https://vue-test-utils.vuejs.org/'
+    }
+    const wrapper = createWrapper({ propsData: { item } })
+    dateNow.mockRestore()
+    expect(wrapper.element).toMatchSnapshot()
+  })
+  test('renders correctly as job', () => {
+    const dateNow = jest.spyOn(Date, 'now')
+    const dateNowTime = new Date('2018')
+
+    dateNow.mockImplementation(() => dateNowTime)
+
+    const item = {
+      by: 'eddyerburgh',
+      id: 11122233,
+      score: 10,
+      time: (dateNowTime / 1000) - 600,
+      title: 'vue-test-utils is released',
+      type: 'job'
+    }
+    const wrapper = createWrapper({
+      propsData: {
+        item
+      }
+    })
+    dateNow.mockRestore()
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
